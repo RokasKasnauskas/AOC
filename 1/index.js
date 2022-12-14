0423d7f1fe14d37f974a675b7ee94b4d6ea585dc
Rokas Kasnauskas <rokas.kasnauskas@gmail.com>
2022-12-14 20:59:32 +0200

    Revert "AOC: day 1"
    This reverts commit 7a3f292d38ee94b3eed77e723c4c737ee0151f4b.

--- 1/index.js
+++
@@ -1,7 +0,0 @@
-const input = ""
-const elfsItemsCalories =  input.split('\n\n').map((elf) => elf.split('\n'))
-const elfsCalories = elfsItemsCalories.map((elf) => elf.reduce((a, b) => Number(a) +Number(b), 0))
-const sortedElfsCaloreis = elfsCalories.sort((a,b) => a - b)
-const elfsWithMostCalories = sortedElfsCaloreis.slice(-3)
-const sumOfMostCalories = elfsWithMostCalories.reduce((a,b) => a + b, 0)
-console.log(sumOfMostCalories)
